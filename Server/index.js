6991cff49b35902e2e691cb345705dce3281e22a
princeroy8606 <83387075+princeroy8606@users.noreply.github.com>
2023-01-04 22:39:12 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 
-const app = express();
+var app = express();
 
 app.use(express.json());
 app.use(helmet());
